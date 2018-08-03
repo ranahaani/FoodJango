f99409cf5f9b6ce186ba06bdd96b35213a84a0cc
Ibrahim Khan <ibii4466@gmail.com>
2018-08-03 14:15:02 +0500

    Initial Commit

---
+++ FoodleDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodleDo
+//
+//  Created by l1f14bscs0033 on 03/08/2018.
+//  Copyright © 2018 l1f14bscs0033. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
